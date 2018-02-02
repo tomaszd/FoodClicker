cdd0f46e1d810a4ffedc2c2240dc816990185397
tomaszd <tomaszdudziec@gmail.com>
2018-02-02 23:34:56 +0100

    added adders /deleters for warzywa orzechy

--- app/src/main/java/com/example/tomaszek/foodclicker/MainActivity.java
+++ app/src/main/java/com/example/tomaszek/foodclicker/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,10 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button btnWoda = (Button) findViewById(R.id.btnWoda);
         final Button btnDelete = (Button) findViewById(R.id.btnDelete);
+        final Button btnWoda = (Button) findViewById(R.id.btnWoda);
         final EditText editWoda = (EditText) findViewById(R.id.editWoda);
-
+        final Button btnWarzywa = (Button) findViewById(R.id.btnWarzywa);
+        final EditText editWarzywa = (EditText) findViewById(R.id.editWarzywa);
+        final Button btnOwoce = (Button) findViewById(R.id.btnOwoce);
+        final EditText editOwoce = (EditText) findViewById(R.id.editOwoce);
+        final Button btnOrzechy = (Button) findViewById(R.id.btnOrzechy);
+        final EditText editOrzechy = (EditText) findViewById(R.id.editOrzechy);
 
         btnWoda.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,15 +33,92 @@ public class MainActivity extends AppCompatActivity {
                 int actualValue = Integer.parseInt(String.valueOf(editWoda.getText()));
                 editWoda.setText(String.valueOf(actualValue + 1));
             }
-
         });
+        btnWoda.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int actualValue = Integer.parseInt(String.valueOf(editWoda.getText()));
+                if (actualValue > 0) {
+                    editWoda.setText(String.valueOf(actualValue - 1));
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        btnWarzywa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int actualValue = Integer.parseInt(String.valueOf(editWarzywa.getText()));
+                editWarzywa.setText(String.valueOf(actualValue + 1));
+            }
+        });
+        btnWarzywa.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int actualValue = Integer.parseInt(String.valueOf(editWarzywa.getText()));
+                if (actualValue > 0) {
+                    editWarzywa.setText(String.valueOf(actualValue - 1));
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        btnOwoce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int actualValue = Integer.parseInt(String.valueOf(editOwoce.getText()));
+                editOwoce.setText(String.valueOf(actualValue + 1));
+            }
+        });
+        btnOwoce.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int actualValue = Integer.parseInt(String.valueOf(editOwoce.getText()));
+                if (actualValue > 0) {
+                    editOwoce.setText(String.valueOf(actualValue - 1));
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+        btnOrzechy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int actualValue = Integer.parseInt(String.valueOf(editOrzechy.getText()));
+                editOrzechy.setText(String.valueOf(actualValue + 1));
+            }
+        });
+        btnOrzechy.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int actualValue = Integer.parseInt(String.valueOf(editOrzechy.getText()));
+                if (actualValue > 0) {
+                    editOrzechy.setText(String.valueOf(actualValue - 1));
+                    return true;
+                }
+                return false;
+            }
+        });
+
 
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 editWoda.setText("0");
+                editWarzywa.setText("0");
+                editOwoce.setText("0");
+                editOrzechy.setText("0");
             }
 
         });
+
+
     }
 }
