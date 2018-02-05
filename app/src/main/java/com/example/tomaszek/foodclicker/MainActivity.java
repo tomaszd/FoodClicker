fee588b18aa421e88daf3849163a3d54411dbb12
tomaszd <tomaszdudziec@gmail.com>
2018-02-06 00:32:27 +0100

    nicer icons

--- app/src/main/java/com/example/tomaszek/foodclicker/MainActivity.java
+++ app/src/main/java/com/example/tomaszek/foodclicker/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.tomaszek.foodclicker;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,27 +18,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button btnDelete = (Button) findViewById(R.id.btnDelete);
-        final Button btnWoda = (Button) findViewById(R.id.btnWoda);
+        final ImageView btnDelete = (ImageView) findViewById(R.id.btnDelete);
+        final ImageView btnWoda = (ImageView) findViewById(R.id.btnWoda);
         final TextView editWoda = (TextView) findViewById(R.id.editWoda);
-        final Button btnInne = (Button) findViewById(R.id.btnInne);
+        final ImageView btnInne = (ImageView) findViewById(R.id.btnInne);
         final TextView editInne = (TextView) findViewById(R.id.editInne);
-        final Button btnWarzywa = (Button) findViewById(R.id.btnWarzywa);
+        final ImageView btnWarzywa = (ImageView) findViewById(R.id.btnWarzywa);
         final TextView editWarzywa = (TextView) findViewById(R.id.editWarzywa);
-        final Button btnOwoce = (Button) findViewById(R.id.btnOwoce);
+        final ImageView btnOwoce = (ImageView) findViewById(R.id.btnOwoce);
         final TextView editOwoce = (TextView) findViewById(R.id.editOwoce);
-        final Button btnRyby = (Button) findViewById(R.id.btnRyby);
+        final ImageView btnRyby = (ImageView) findViewById(R.id.btnRyby);
         final TextView editRyby = (TextView) findViewById(R.id.editRyby);
-        final Button btnMieso = (Button) findViewById(R.id.btnMieso);
-        final TextView editMieso = (TextView) findViewById(R.id.editMieso);
-        final Button btnZboza = (Button) findViewById(R.id.btnZboza);
+        final ImageView btnZboza = (ImageView) findViewById(R.id.btnZboza);
         final TextView editZboza = (TextView) findViewById(R.id.editZboza);
-        final Button btnNabial = (Button) findViewById(R.id.btnNabial);
+        final ImageView btnNabial = (ImageView) findViewById(R.id.btnNabial);
         final TextView editNabial = (TextView) findViewById(R.id.editNabial);
-        final Button btnOrzechy = (Button) findViewById(R.id.btnOrzechy);
+        final ImageView btnOrzechy = (ImageView) findViewById(R.id.btnOrzechy);
         final TextView editOrzechy = (TextView) findViewById(R.id.editOrzechy);
-        final Button btnSlodycze = (Button) findViewById(R.id.btnSlodycze);
-        final TextView editSlodycze = (TextView) findViewById(R.id.editSlodycze);
+        final ImageView btnPiramida = (ImageView) findViewById(R.id.btnPiramida);
+
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        final SharedPreferences.Editor editor = settings.edit();
+
+
+        int woda_value = settings.getInt("woda", 0);
+        int inne_value = settings.getInt("inne", 0);
+        int warzywa_value = settings.getInt("warzywa", 0);
+        int owoce_value = settings.getInt("owoce", 0);
+        int zboza_value = settings.getInt("zboza", 0);
+        int ryby_value = settings.getInt("ryby", 0);
+        int nabial_value = settings.getInt("nabial", 0);
+        int orzech_value = settings.getInt("orzechy", 0);
+
+        setActualValue(editWoda, editInne, editWarzywa, editOwoce, editRyby, editZboza, editNabial, editOrzechy, woda_value, inne_value, warzywa_value, owoce_value, zboza_value, ryby_value, nabial_value, orzech_value);
+
 
         btnWoda.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
 
                 int actualValue = Integer.parseInt(String.valueOf(editWoda.getText()));
                 editWoda.setText(String.valueOf(actualValue + 1));
+                saveSharedPref(editor, "woda", actualValue);
+
             }
         });
         btnWoda.setOnLongClickListener(new View.OnLongClickListener() {
@@ -52,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
                 int actualValue = Integer.parseInt(String.valueOf(editWoda.getText()));
                 if (actualValue > 0) {
                     editWoda.setText(String.valueOf(actualValue - 1));
+                    saveSharedPref(editor, "woda", actualValue);
                     return true;
                 }
                 return false;
@@ -64,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
                 int actualValue = Integer.parseInt(String.valueOf(editInne.getText()));
                 editInne.setText(String.valueOf(actualValue + 1));
+                saveSharedPref(editor, "inne", actualValue);
             }
         });
         btnInne.setOnLongClickListener(new View.OnLongClickListener() {
@@ -72,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
                 int actualValue = Integer.parseInt(String.valueOf(editInne.getText()));
                 if (actualValue > 0) {
                     editInne.setText(String.valueOf(actualValue - 1));
+                    saveSharedPref(editor, "inne", actualValue);
                     return true;
                 }
                 return false;
@@ -84,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
 
                 int actualValue = Integer.parseInt(String.valueOf(editWarzywa.getText()));
                 editWarzywa.setText(String.valueOf(actualValue + 1));
+                saveSharedPref(editor, "warzywa", actualValue);
             }
         });
         btnWarzywa.setOnLongClickListener(new View.OnLongClickListener() {
@@ -92,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
                 int actualValue = Integer.parseInt(String.valueOf(editWarzywa.getText()));
                 if (actualValue > 0) {
                     editWarzywa.setText(String.valueOf(actualValue - 1));
+                    saveSharedPref(editor, "warzywa", actualValue);
                     return true;
                 }
                 return false;
@@ -104,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
 
                 int actualValue = Integer.parseInt(String.valueOf(editOwoce.getText()));
                 editOwoce.setText(String.valueOf(actualValue + 1));
+                saveSharedPref(editor, "owoce", actualValue);
             }
         });
         btnOwoce.setOnLongClickListener(new View.OnLongClickListener() {
@@ -112,6 +136,7 @@ public class MainActivity extends AppCompatActivity {
                 int actualValue = Integer.parseInt(String.valueOf(editOwoce.getText()));
                 if (actualValue > 0) {
                     editOwoce.setText(String.valueOf(actualValue - 1));
+                    saveSharedPref(editor, "owoce", actualValue);
                     return true;
                 }
                 return false;
@@ -124,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
 
                 int actualValue = Integer.parseInt(String.valueOf(editRyby.getText()));
                 editRyby.setText(String.valueOf(actualValue + 1));
+                saveSharedPref(editor, "ryby", actualValue);
             }
         });
         btnRyby.setOnLongClickListener(new View.OnLongClickListener() {
@@ -132,25 +158,7 @@ public class MainActivity extends AppCompatActivity {
                 int actualValue = Integer.parseInt(String.valueOf(editRyby.getText()));
                 if (actualValue > 0) {
                     editRyby.setText(String.valueOf(actualValue - 1));
-                    return true;
-                }
-                return false;
-            }
-        });
-        btnMieso.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                int actualValue = Integer.parseInt(String.valueOf(editMieso.getText()));
-                editMieso.setText(String.valueOf(actualValue + 1));
-            }
-        });
-        btnMieso.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                int actualValue = Integer.parseInt(String.valueOf(editMieso.getText()));
-                if (actualValue > 0) {
-                    editMieso.setText(String.valueOf(actualValue - 1));
+                    saveSharedPref(editor, "ryby", actualValue);
                     return true;
                 }
                 return false;
@@ -163,6 +171,7 @@ public class MainActivity extends AppCompatActivity {
 
                 int actualValue = Integer.parseInt(String.valueOf(editZboza.getText()));
                 editZboza.setText(String.valueOf(actualValue + 1));
+                saveSharedPref(editor, "zboza", actualValue);
             }
         });
         btnZboza.setOnLongClickListener(new View.OnLongClickListener() {
@@ -171,6 +180,7 @@ public class MainActivity extends AppCompatActivity {
                 int actualValue = Integer.parseInt(String.valueOf(editZboza.getText()));
                 if (actualValue > 0) {
                     editZboza.setText(String.valueOf(actualValue - 1));
+                    saveSharedPref(editor, "zboza", actualValue);
                     return true;
                 }
                 return false;
@@ -183,6 +193,7 @@ public class MainActivity extends AppCompatActivity {
 
                 int actualValue = Integer.parseInt(String.valueOf(editNabial.getText()));
                 editNabial.setText(String.valueOf(actualValue + 1));
+                saveSharedPref(editor, "nabial", actualValue);
             }
         });
         btnNabial.setOnLongClickListener(new View.OnLongClickListener() {
@@ -191,18 +202,20 @@ public class MainActivity extends AppCompatActivity {
                 int actualValue = Integer.parseInt(String.valueOf(editNabial.getText()));
                 if (actualValue > 0) {
                     editNabial.setText(String.valueOf(actualValue - 1));
+                    saveSharedPref(editor, "nabial", actualValue);
                     return true;
                 }
                 return false;
             }
         });
-        
+
         btnOrzechy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 int actualValue = Integer.parseInt(String.valueOf(editOrzechy.getText()));
                 editOrzechy.setText(String.valueOf(actualValue + 1));
+                saveSharedPref(editor, "orzechy", actualValue);
             }
         });
         btnOrzechy.setOnLongClickListener(new View.OnLongClickListener() {
@@ -211,28 +224,27 @@ public class MainActivity extends AppCompatActivity {
                 int actualValue = Integer.parseInt(String.valueOf(editOrzechy.getText()));
                 if (actualValue > 0) {
                     editOrzechy.setText(String.valueOf(actualValue - 1));
+                    saveSharedPref(editor, "orzechy", actualValue);
                     return true;
                 }
                 return false;
             }
         });
-        btnSlodycze.setOnClickListener(new View.OnClickListener() {
+
+        btnPiramida.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                int actualValue = Integer.parseInt(String.valueOf(editSlodycze.getText()));
-                editSlodycze.setText(String.valueOf(actualValue + 1));
-            }
-        });
-        btnSlodycze.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                int actualValue = Integer.parseInt(String.valueOf(editSlodycze.getText()));
-                if (actualValue > 0) {
-                    editSlodycze.setText(String.valueOf(actualValue - 1));
-                    return true;
-                }
-                return false;
+                Intent myIntent = new Intent(MainActivity.this, PiramidActivity.class);
+                myIntent.putExtra("woda", ""); //Optional parameters
+                myIntent.putExtra("inne", ""); //Optional parameters
+                myIntent.putExtra("warzywa", ""); //Optional parameters
+                myIntent.putExtra("owoce", ""); //Optional parameters
+                myIntent.putExtra("ryby", ""); //Optional parameters
+                myIntent.putExtra("zboza", ""); //Optional parameters
+                myIntent.putExtra("nabial", ""); //Optional parameters
+                myIntent.putExtra("orzechy", ""); //Optional parameters
+                MainActivity.this.startActivity(myIntent);
             }
         });
 
@@ -244,17 +256,29 @@ public class MainActivity extends AppCompatActivity {
                 editWarzywa.setText("0");
                 editOwoce.setText("0");
                 editRyby.setText("0");
-                editMieso.setText("0");
                 editZboza.setText("0");
                 editNabial.setText("0");
                 editOrzechy.setText("0");
-                editSlodycze.setText("0");
-
-
             }
 
         });
 
 
+    }
+
+    private void saveSharedPref(SharedPreferences.Editor editor, String key, int actual_value) {
+        editor.putInt(key, actual_value);
+        editor.commit();
+    }
+
+    private void setActualValue(TextView editWoda, TextView editInne, TextView editWarzywa, TextView editOwoce, TextView editRyby, TextView editZboza, TextView editNabial, TextView editOrzechy, int woda_value, int inne_value, int warzywa_value, int owoce_value, int zboza_value, int ryby_value, int nabial_value, int orzech_value) {
+        editWoda.setText(String.valueOf(woda_value));
+        editInne.setText(String.valueOf(inne_value));
+        editWarzywa.setText(String.valueOf(warzywa_value));
+        editOwoce.setText(String.valueOf(owoce_value));
+        editZboza.setText(String.valueOf(zboza_value));
+        editRyby.setText(String.valueOf(ryby_value));
+        editNabial.setText(String.valueOf(nabial_value));
+        editOrzechy.setText(String.valueOf(orzech_value));
     }
 }
