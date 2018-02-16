d855cd5c4e2b87e8811374dbe063bd9c3961ce3c
tomaszd <tomaszdudziec@gmail.com>
2018-02-17 00:11:52 +0100

    added TaelaActivity

--- app/src/main/java/com/example/tomaszek/foodclicker/TabelaActivity.java
+++ app/src/main/java/com/example/tomaszek/foodclicker/TabelaActivity.java
@@ -1,7 +1,10 @@
 package com.example.tomaszek.foodclicker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class TabelaActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class TabelaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabela);
+
+        ImageView btnWoda = (ImageView) findViewById(R.id.btnWoda);
+        btnWoda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(TabelaActivity.this, WodaDetailsActivity.class);
+                TabelaActivity.this.startActivity(myIntent);
+            }
+        });
     }
+
+
 }
