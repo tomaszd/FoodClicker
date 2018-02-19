0e5a6f7b6f5424cd79fd3333444f941fd85a2077
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2018-02-19 11:34:14 +0100

    added button support

--- app/src/main/java/com/example/tomaszek/foodclicker/TabelaActivity.java
+++ app/src/main/java/com/example/tomaszek/foodclicker/TabelaActivity.java
@@ -15,7 +15,7 @@ public class TabelaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabela);
 
-        EditText appCompatImageButton =(EditText) findViewById(R.id.appCompatImageButton);
+        EditText appCompatImageButton = (EditText) findViewById(R.id.appCompatImageButton);
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/custom_font.ttf");
         appCompatImageButton.setTypeface(typeface);
 
@@ -38,6 +38,59 @@ public class TabelaActivity extends AppCompatActivity {
             }
         });
 
+        ImageView btnWarzywa = (ImageView) findViewById(R.id.btnWarzywa);
+        btnWarzywa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(TabelaActivity.this, WarzywaDetailsActivity.class);
+                TabelaActivity.this.startActivity(myIntent);
+            }
+        });
+
+        ImageView btnOwoce = (ImageView) findViewById(R.id.btnOwoce);
+        btnOwoce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(TabelaActivity.this, OwoceDetailsActivity.class);
+                TabelaActivity.this.startActivity(myIntent);
+            }
+        });
+
+        ImageView btnRyby = (ImageView) findViewById(R.id.btnRyby);
+        btnRyby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(TabelaActivity.this, RybyDetailsActivity.class);
+                TabelaActivity.this.startActivity(myIntent);
+            }
+        });
+
+        ImageView btnOlejorzech = (ImageView) findViewById(R.id.btnOlejorzech);
+        btnOlejorzech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(TabelaActivity.this, OlejorzechDetailsActivity.class);
+                TabelaActivity.this.startActivity(myIntent);
+            }
+        });
+
+        ImageView btnZboza = (ImageView) findViewById(R.id.btnZboza);
+        btnZboza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(TabelaActivity.this, ZbozaDetailsActivity.class);
+                TabelaActivity.this.startActivity(myIntent);
+            }
+        });
+
+        ImageView btnNabial = (ImageView) findViewById(R.id.btnNabial);
+        btnNabial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(TabelaActivity.this, NabialDetailsActivity.class);
+                TabelaActivity.this.startActivity(myIntent);
+            }
+        });
 
     }
 
