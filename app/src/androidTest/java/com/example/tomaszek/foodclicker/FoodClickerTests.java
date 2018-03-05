619e0edfc82d9cbe3c9ba1279a21c761e1248563
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2018-03-05 08:44:07 +0100

    added test for start of Activites

--- app/src/androidTest/java/com/example/tomaszek/foodclicker/FoodClickerTests.java
+++ app/src/androidTest/java/com/example/tomaszek/foodclicker/FoodClickerTests.java
@@ -44,14 +44,70 @@ public class FoodClickerTests {
             false);   // launchActivity. False to customize the intent
 
 
-
     @Rule
     public ActivityTestRule<SettingsActivity> settingsActivityRule
             = new ActivityTestRule<>(
             SettingsActivity.class,
             true,     // initialTouchMode
             false);   // launchActivity. False to customize the intent
-    
+
+    @Rule
+    public ActivityTestRule<InneDetailsActivity> inneDetailsActivityRule
+            = new ActivityTestRule<>(
+            InneDetailsActivity.class,
+            true,     // initialTouchMode
+            false);   // launchActivity. False to customize the intent
+
+    @Rule
+    public ActivityTestRule<NabialDetailsActivity> nabialDetailsActivityRule
+            = new ActivityTestRule<>(
+            NabialDetailsActivity.class,
+            true,     // initialTouchMode
+            false);   // launchActivity. False to customize the intent
+
+    @Rule
+    public ActivityTestRule<OlejorzechDetailsActivity> olejorzechDetailsActivityTestRule
+            = new ActivityTestRule<>(
+            OlejorzechDetailsActivity.class,
+            true,     // initialTouchMode
+            false);   // launchActivity. False to customize the intent
+
+    @Rule
+    public ActivityTestRule<RybyDetailsActivity> rybyDetailsActivityTestRule
+            = new ActivityTestRule<>(
+            RybyDetailsActivity.class,
+            true,     // initialTouchMode
+            false);   // launchActivity. False to customize the intent
+
+
+    @Rule
+    public ActivityTestRule<OwoceDetailsActivity> owoceDetailsActivityTestRule
+            = new ActivityTestRule<>(
+            OwoceDetailsActivity.class,
+            true,     // initialTouchMode
+            false);   // launchActivity. False to customize the intent
+
+    @Rule
+    public ActivityTestRule<WarzywaDetailsActivity> warzywaDetailsActivityTestRule
+            = new ActivityTestRule<>(
+            WarzywaDetailsActivity.class,
+            true,     // initialTouchMode
+            false);   // launchActivity. False to customize the intent
+
+    @Rule
+    public ActivityTestRule<WodaDetailsActivity> wodaDetailsActivityActivityTestRule
+            = new ActivityTestRule<>(
+            WodaDetailsActivity.class,
+            true,     // initialTouchMode
+            false);   // launchActivity. False to customize the intent
+
+    @Rule
+    public ActivityTestRule<ZbozaDetailsActivity> zbozaDetailsActivityTestRule
+            = new ActivityTestRule<>(
+            ZbozaDetailsActivity.class,
+            true,     // initialTouchMode
+            false);   // launchActivity. False to customize the intent
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
@@ -59,6 +115,7 @@ public class FoodClickerTests {
         assertEquals("com.example.tomaszek.foodclicker", appContext.getPackageName());
 
     }
+
 
     @Test
     public void startMainActivity() throws Exception {
@@ -68,28 +125,72 @@ public class FoodClickerTests {
     }
 
     @Test
-    public void startPiramidActivityFromMainActivity() throws Exception {
+    public void startPiramidActivityStartWithoutCrash() throws Exception {
         Intent intent = new Intent();
         piramidActivityRule.launchActivity(intent);
 
     }
 
     @Test
-    public void startTabelaActivityFromMainActivity() throws Exception {
+    public void startTabelaActivityStartWithoutCrash() throws Exception {
         Intent intent = new Intent();
         tabelaActivityRule.launchActivity(intent);
 
     }
 
     @Test
-    public void startSettingsActivityFromMainActivity() throws Exception {
+    public void startSettingsActivityStartWithoutCrash() throws Exception {
         Intent intent = new Intent();
         settingsActivityRule.launchActivity(intent);
 
     }
 
+    @Test
+    public void inneDetailsStartWithoutCrash() throws Exception {
+        Intent intent = new Intent();
+        inneDetailsActivityRule.launchActivity(intent);
 
+    }
 
+    @Test
+    public void nabialDetailsActivityStartWithoutCrash() throws Exception {
+        Intent intent = new Intent();
+        nabialDetailsActivityRule.launchActivity(intent);
+    }
 
+    @Test
+    public void olejorzechDetailsStartWithoutCrash() throws Exception {
+        Intent intent = new Intent();
+        olejorzechDetailsActivityTestRule.launchActivity(intent);
+    }
 
+    @Test
+    public void owoceDetailsActivityTestRuleStartWithoutCrash() throws Exception {
+        Intent intent = new Intent();
+        owoceDetailsActivityTestRule.launchActivity(intent);
+    }
+
+    @Test
+    public void rybyDetailsActivityStartWithoutCrash() throws Exception {
+        Intent intent = new Intent();
+        rybyDetailsActivityTestRule.launchActivity(intent);
+    }
+
+    @Test
+    public void warzywaDetailsActivityStartWithoutCrash() throws Exception {
+        Intent intent = new Intent();
+        warzywaDetailsActivityTestRule.launchActivity(intent);
+    }
+
+    @Test
+    public void wodaDetailsActivityStartWithoutCrash() throws Exception {
+        Intent intent = new Intent();
+        wodaDetailsActivityActivityTestRule.launchActivity(intent);
+    }
+
+    @Test
+    public void zbozaDetailsActivityStartWithoutCrash() throws Exception {
+        Intent intent = new Intent();
+        zbozaDetailsActivityTestRule.launchActivity(intent);
+    }
 }
